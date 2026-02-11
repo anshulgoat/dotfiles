3b76a4efe3f83a2047a50d60400f8ed0fd511e4c
anshul <108891162+anshulgoat@users.noreply.github.com>
2026-02-12 01:25:29 +0530

    Delete .config directory

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-  set fish_greeting
- starship init fish | source 
- # Commands to run in interactive sessions can go here
-end
-export PATH="$HOME/.local/bin:$PATH"
-pfetch 
